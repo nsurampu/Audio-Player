86f58d4b3c0069de30c2efdf79e7a0587b1eb591
Naren Surampudi <nsurampu@gmail.com>
2017-09-01 21:19:05 +0000

    Add files via upload

---
+++ player.py
@@ -0,0 +1,38 @@
+#An attempt to build an audio player with basic functionality
+
+import pygame
+import os
+from random import randint
+
+music_path = raw_input('Enter the complete path of required music folder: ')
+file_array = os.listdir(music_path)
+audio_array = []
+
+for i in range(0, len(file_array)):
+    if file_array[i].split('.')[1] == 'mp3':
+        audio_array.append(file_array[i])
+
+pygame.init()
+screen = pygame.display.set_mode((500, 500))
+screen.fill([0, 0, 0])
+pygame.display.update()
+pygame.display.set_caption('Audio Player')
+
+pygame.mixer.init(frequency = 22050, size = -16, channels = 2, buffer = 4096)
+
+for i in range(0, len(audio_array)):
+    pygame.mixer.music.load(audio_array[i])
+    pygame.mixer.music.play()
+
+    while True:
+        if pygame.mixer.music.get_busy():
+            red = randint(0, 255)
+            green = randint(0, 255)
+            blue = randint(0, 255)
+            screen.fill([red, green, blue])
+            pygame.display.update()
+            print('Audio Player is running')
+        else:
+            print('Audio Player has stopped')
+
+pygame.quit()

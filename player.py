caa386ac4492ebce43591354b63487baaef8ec49
Naren Surampudi <nsurampu@gmail.com>
2017-09-06 11:33:33 +0530

    Update player.py

--- player.py
+++ player.py
@@ -4,13 +4,13 @@ import pygame
 import os
 from random import randint
 
-music_path = os.getcwd()
+music_path = os.getcwd()    #Gets the current working directory
 file_array = os.listdir(music_path)
 audio_array = []
 audio_name = 'default'
 
 for i in range(0, len(file_array)):
-    if file_array[i].split('.')[1] == 'mp3':
+    if file_array[i].split('.')[1] == 'mp3':    #Gets all mp3 files
         audio_array.append(file_array[i])
 
 pygame.init()
@@ -19,12 +19,12 @@ screen.fill([0, 0, 0])
 pygame.display.update()
 pygame.display.set_caption('Audio Player')
 
-pygame.mixer.init(frequency = 22050, size = -16, channels = 2, buffer = 4096)
+pygame.mixer.init(frequency = 22050, size = -16, channels = 2, buffer = 4096)   #Initializing mixer with basic stuff to avoid sound lag
 
 for i in range(0, len(audio_array)):
     pygame.mixer.music.load(audio_array[i])
     audio_name = audio_array[i].split('.')[0]
-    pygame.display.set_caption('Audio Player: ' + audio_name)
+    pygame.display.set_caption('Audio Player: ' + audio_name)   #Displaying file name in the window caption
     pygame.mixer.music.play()
 
     while True:
@@ -32,7 +32,7 @@ for i in range(0, len(audio_array)):
             red = randint(0, 255)
             green = randint(0, 255)
             blue = randint(0, 255)
-            screen.fill([red, green, blue])
+            screen.fill([red, green, blue])     #Displays multi-color transition
             pygame.display.update()
             print('Audio Player is running')
         else:
